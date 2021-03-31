487f457b2a19c55929a214b92d82e50367580b89
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-03-31 12:37:28 -0500

    Add template.

---
+++ 00_template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
