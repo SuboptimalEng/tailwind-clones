83faa1d2e9bc33d98e70a040b58d7878b4ce2cc9
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-03-31 12:51:23 -0500

    Update template.

--- 00_template/src/main.js
+++ 00_template/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import './assets/tailwind.css';
 
-createApp(App).mount('#app');
+library.add(fas, fab);
+
+createApp(App)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app');
