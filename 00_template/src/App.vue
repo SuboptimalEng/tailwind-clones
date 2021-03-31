187cf50184a5053b2ee6376d00745cb927916d7b
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-03-31 12:59:55 -0500

    Minor changes.

--- 00_template/src/App.vue
+++ 00_template/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full h-full min-h-screen">
-    <div class="mx-auto my-auto text-6xl">
-      <fa icon="home" class="hover:text-gray-400" />
+    <div class="mx-auto pt-16 text-6xl">
+      <fa icon="coffee" class="hover:text-gray-400" />
       <fa :icon="['fab', 'youtube']" class="hover:text-gray-400" />
     </div>
   </div>
