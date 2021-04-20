5704a7f8159718ffd149f688646d9169eb677aad
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-04-20 10:58:33 -0500

    add messenger.

---
+++ 04_messenger/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex w-full h-full min-h-screen">
+    <div class="mx-auto pt-16 text-6xl">
+      <fa icon="coffee" class="hover:text-gray-400" />
+      <fa :icon="['fab', 'youtube']" class="hover:text-gray-400" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
