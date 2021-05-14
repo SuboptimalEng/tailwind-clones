23579f87ba6d6996e957b762c8c68215e86aca76
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-05-14 15:07:31 -0500

    get extension images for thumbnail.

--- 06_vscode/src/App.vue
+++ 06_vscode/src/App.vue
@@ -32,7 +32,7 @@
 
     <!-- extensions -->
     <div
-      class="flex absolute top-0 bottom-10 left-20 w-96 bg-editor-bg text-sidebar-fg"
+      class="flex absolute top-0 bottom-10 left-20 w-96 bg-explorer-bg text-sidebar-fg"
     >
       <div
         class="flex flex-col absolute w-full top-0 bottom-0 overflow-y-hidden p-4 space-y-4"
@@ -61,8 +61,8 @@
             :key="extension.name"
             class="flex justify-between"
           >
-            <div class="py-2 -ml-2 flex w-full items-center -space-x-2">
-              <div class="w-40">
+            <div class="py-2 flex w-full items-center space-x-4">
+              <div class="w-28">
                 <img :src="extension.image" alt="" class="rounded-full" />
               </div>
               <div class="flex flex-col w-full">
@@ -79,6 +79,68 @@
                 </div>
                 <div class="font-bold">
                   {{ extension.creator }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="absolute ml-20 left-96 top-0 right-0 bottom-10 bg-editor-bg">
+      <div class="flex">
+        <div
+          class="flex items-center justify-between w-64 p-2 px-4 bg-editor-bg text-off-white"
+        >
+          <div class="flex items-center space-x-2">
+            <img src="/images/vscode.png" alt="" class="w-8" />
+            <div>Getting Started</div>
+          </div>
+          <div class="pb-1">x</div>
+        </div>
+        <div class="w-full h-30 bg-explorer-bg"></div>
+      </div>
+    </div>
+
+    <div
+      class="absolute ml-20 left-96 top-10 right-0 bottom-10 bg-editor-bg text-off-white"
+    >
+      <div class="p-36 flex flex-col space-y-10">
+        <div class="flex flex-col space-y-2">
+          <div class="text-6xl">Visual Studio Code</div>
+          <div class="text-4xl text-sidebar-fg">Editing evolved</div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div>
+            <div class="text-2xl pb-4">Start</div>
+            <div class="flex flex-col space-y-2">
+              <div
+                v-for="option in startOptions"
+                :key="option.name"
+                class="flex space-x-2 text-lg"
+              >
+                <div class="w-8">
+                  <fa :icon="option.icon" />
+                </div>
+                <div class="text-activity-bg">
+                  {{ option.name }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="text-2xl pb-4">Getting Started</div>
+            <div class="flex flex-col space-y-2 text-xl">
+              <div
+                v-for="option in gettingStartedOptions"
+                :key="option.name"
+                class="rounded-xl bg-sidebar-bg p-4 px-8 flex space-x-4"
+              >
+                <div class="w-8 text-activity-bg">
+                  <fa :icon="option.icon" />
+                </div>
+                <div>
+                  {{ option.name }}
                 </div>
               </div>
             </div>
@@ -130,6 +192,11 @@ import {
   faBell,
   faFilter,
   faEllipsisH,
+  faFolderOpen,
+  faPalette,
+  faBolt,
+  faLightbulb,
+  faGraduationCap,
 } from '@fortawesome/free-solid-svg-icons';
 
 export default {
@@ -206,56 +273,95 @@ export default {
           image: '/images/dracula.png',
           downloads: '2.2M',
           description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          creator: '@zenorocha',
+        },
+        {
+          name: 'VS Code Tinder',
+          image: '/images/vsinder.png',
+          downloads: '2.2M',
+          description: 'A dating app for developers...',
+          creator: '@benawad',
+        },
+        {
+          name: 'Doge House',
+          image: '/images/dogehouse.png',
+          downloads: '2.2M',
+          description: 'Voice convos to the moon...',
+          creator: '@benawad',
+        },
+        {
+          name: 'Material Icons',
+          image: '/images/material.png',
+          downloads: '2.2M',
+          description: 'Material Design Icons for VS...',
+          creator: '@PhilippKief',
+        },
+        {
+          name: 'Docker',
+          image: '/images/docker.png',
+          downloads: '2.2M',
+          description: 'Makes it easy to create, manage...',
+          creator: '@Microsoft',
+        },
+        {
+          name: 'C#',
+          image: '/images/csharp.png',
+          downloads: '2.2M',
+          description: 'Makes it easy to create, manage...',
+          creator: '@Microsoft',
         },
         {
           name: 'Dracula Official',
           image: '/images/dracula.png',
           downloads: '2.2M',
           description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          creator: '@zenorocha',
         },
         {
           name: 'Dracula Official',
           image: '/images/dracula.png',
           downloads: '2.2M',
           description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          creator: '@zenorocha',
         },
         {
           name: 'Dracula Official',
           image: '/images/dracula.png',
           downloads: '2.2M',
           description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          creator: '@zenorocha',
+        },
+      ],
+      startOptions: [
+        {
+          name: 'New File',
+          icon: faFile,
         },
         {
-          name: 'Dracula Official',
-          image: '/images/dracula.png',
-          downloads: '2.2M',
-          description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          name: 'Open',
+          icon: faFolderOpen,
         },
         {
-          name: 'Dracula Official',
-          image: '/images/dracula.png',
-          downloads: '2.2M',
-          description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          name: 'Clone Git Repo...',
+          icon: faCodeBranch,
         },
         {
-          name: 'Dracula Official',
-          image: '/images/dracula.png',
-          downloads: '2.2M',
-          description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          name: 'Run a Command...',
+          icon: faPalette,
+        },
+      ],
+      gettingStartedOptions: [
+        {
+          name: 'Customize your Setup',
+          icon: faBolt,
         },
         {
-          name: 'Dracula Official',
-          image: '/images/dracula.png',
-          downloads: '2.2M',
-          description: 'Official Dracula Theme. A dark...',
-          creator: 'Dracula Theme',
+          name: 'Learn the Fundamentals',
+          icon: faLightbulb,
+        },
+        {
+          name: 'Boost your Productivity',
+          icon: faGraduationCap,
         },
       ],
     };
